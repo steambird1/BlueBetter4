ec4ec2eaad26991383f4c267590c8d31946bcbb9
steambird1 <steambird@163.com>
2023-04-01 20:35:43 +0800

    Version information updates

--- main.cpp
+++ main.cpp
@@ -3254,7 +3254,7 @@ int main(int argc, char* argv[]) {
 	in_debug = false;
 	no_lib = false;
 #endif
-	string version_info = string("BlueBetter Interpreter\nVersion 1.23\nCompiled on ") + __DATE__ + " " + __TIME__;
+	string version_info = string("BlueBetter Interpreter\nVersion 1.24\nCompiled on ") + __DATE__ + " " + __TIME__;
 #pragma endregion
 	// End
 
