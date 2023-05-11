c36bf887f963cc44bb1e63452f78ac8fe994376a
steambird1 <steambird@163.com>
2023-05-11 22:03:41 +0800

    Bug fixes

--- blue-execute.h
+++ blue-execute.h
@@ -2304,7 +2304,7 @@ else if_have_additional_op('<') {
 		// End
 
 		vector<string> sc = split(code, '\n', -1, '\"', '\\', true);
-		codestream.insert(codestream.end() - 1, sc.begin(), sc.end());
+		if (codestream.size()) codestream.insert(codestream.end() - 1, sc.begin(), sc.end());
 		string curclass = "";					// Will append '.'
 		string curfun = "", cfname = "", cfargs = "";
 		int fun_indent = max_indent;
