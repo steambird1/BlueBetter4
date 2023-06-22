48ec2bbb8f064a9ebc8ab27d276e4e3aaeebfc57
steambird1 <77623231+steambird1@users.noreply.github.com>
2023-06-22 13:58:07 +0800

    Update main.cpp
    Sync version info

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]) {
 	ip.in_debug = false;
 	ip.no_lib = false;
 #endif
-	string version_info = string("BlueBetter Interpreter\nVersion 1.26\nCompiled on ") + __DATE__ + " " + __TIME__;
+	string version_info = string("BlueBetter Interpreter\nVersion 1.27\nCompiled on ") + __DATE__ + " " + __TIME__;
 #pragma endregion
 	// End
 
